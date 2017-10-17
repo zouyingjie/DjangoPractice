c816d0a00f9994c11ca39270545954321412fd12
zouyingjie <yingjiezou1@gmail.com>
2017-10-17 20:30:47 +0800

    init static file

--- DjangoPractice/settings.py
+++ DjangoPractice/settings.py
@@ -14,8 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_HOME, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
